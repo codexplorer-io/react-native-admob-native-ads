57e9fded0985dcad7fe5f008921572c3cd6eb514
Marko Matić <markomatic@users.noreply.github.com>
2024-06-02 16:06:43 +1000

    Update withAdmobNativeAdsInfoPlist.ts

--- plugin/src/withAdmobNativeAdsInfoPlist.ts
+++ plugin/src/withAdmobNativeAdsInfoPlist.ts
@@ -3,7 +3,9 @@ import { withInfoPlist } from '@expo/config-plugins';
 const withAdmobNativeAdsInfoPlist = (config: any, props: any) => {
     return withInfoPlist(config, (config: any) => {
         config.modResults.GADApplicationIdentifier = props.iosAppId;
-        config.modResults.SKAdNetworkItems = skAdNetworkItems;
+        config.modResults.SKAdNetworkItems = skAdNetworkItems.map((item: string) => ({
+            SKAdNetworkIdentifier: item
+        }));
         return config;
     });
 };
